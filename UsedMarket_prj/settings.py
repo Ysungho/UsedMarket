e2d18f1a81e9fb4d087d31e320f4188b86b2cccd
Ysungho <ysh01022320@gmail.com>
2023-11-13 17:57:30 +0900

    docker 까지 완성

--- UsedMarket_prj/settings.py
+++ UsedMarket_prj/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-6+@4pc6klvrmh9jqfdp2m6)_0ov82ltitv^z=es_%88^7-zued')
+SECRET_KEY = os.environ.get('SECRET_KEY','()ugw82!bn40lae_f51h9-q5^9^6kcy9y+$lgn=tykh1u&2w+1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 1))
