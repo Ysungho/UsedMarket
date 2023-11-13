bccf66db7066bec32e7ee38b315a1bb16d867041
Ysungho <ysh01022320@gmail.com>
2023-11-13 16:41:32 +0900

    프로젝트 완성

--- UsedMarket_prj/asgi.py
+++ UsedMarket_prj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for UsedMarket_prj project.
+ASGI config for freeColumn_prj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UsedMarket_prj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freeColumn_prj.settings')
 
 application = get_asgi_application()
--- UsedMarket_prj/wsgi.py
+++ UsedMarket_prj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for UsedMarket_prj project.
+WSGI config for freeColumn_prj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UsedMarket_prj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freeColumn_prj.settings')
 
 application = get_wsgi_application()
